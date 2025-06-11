f4b7494307e4f662efd23ff4b09fd273a6319ae4
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-06-11 13:47:55 -0700

    implemented wave1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry.jsx';
 
 const App = () => {
+  const DATA = {
+    id: 1,
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00',
+    liked: false,
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/
+        <ChatEntry message={DATA} />
+        /*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
